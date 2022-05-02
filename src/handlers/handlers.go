63d223507fad1d6cf68bddd96c7ae63e972746be
delvh <leon@kske.dev>
2022-05-02 16:28:58 +0200

    Add health endpoint with pseudo data

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -3,14 +3,9 @@ package handlers
 import (
 	"github.com/Gamify-IT/login-backend/src/gen/db"
 	"github.com/Gamify-IT/login-backend/src/gen/restapi/operations"
-	"github.com/Gamify-IT/login-backend/src/gen/restapi/operations/todos"
-	"github.com/go-openapi/runtime/middleware"
 )
 
 func ConfigureAPI(api *operations.LoginAPI, client *db.PrismaClient) {
-	if api.TodosGetHandler == nil {
-		api.TodosGetHandler = todos.GetHandlerFunc(func(params todos.GetParams) middleware.Responder {
-			return middleware.NotImplemented("operation todos.Get has not yet been implemented")
-		})
-	}
+
+	api.HealthGetHealthHandler = healthHandler()
 }

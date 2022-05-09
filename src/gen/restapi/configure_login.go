495162960508de381dd450f5691170bc10ed7a6a
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-05-09 12:12:22 +0200

    add cors default handler

--- src/gen/restapi/configure_login.go
+++ src/gen/restapi/configure_login.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Gamify-IT/login-backend/src/gen/db"
 	"github.com/Gamify-IT/login-backend/src/gen/restapi/operations"
 	"github.com/Gamify-IT/login-backend/src/handlers"
+	"github.com/rs/cors"
 	"net/http"
 
 	"github.com/go-openapi/errors"
@@ -75,5 +76,6 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
 // So this is a good place to plug in a panic handling middleware, logging and metrics.
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
-	return handler
+	handleCORS := cors.Default().Handler
+	return handleCORS(handler)
 }

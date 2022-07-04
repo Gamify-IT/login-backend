8caf470a0c2151a9512a9047626a256f811a9e90
Jonathan Scholz <contact@jonathan-scholz.de>
2022-07-04 13:50:32 +0200

    actually check token validity

--- src/handlers/auth/authenticator.go
+++ src/handlers/auth/authenticator.go
@@ -27,14 +27,14 @@ func (a *Authenticator) CookieName() string {
 // GenerateTokenCookie returns a cookie header with a new JSON Web Token.
 // The token is valid for the configured validityDuration.
 // It contains provided the (user) id and name as payload.
-func (j *Authenticator) GenerateTokenCookie(id, name string) (string, error) {
-	token, err := j.GenerateJWT(id, name)
+func (a *Authenticator) GenerateTokenCookie(id, name string) (string, error) {
+	token, err := a.GenerateJWT(id, name)
 
 	if err != nil {
 		return "", err
 	}
 
-	maxAge := int(j.validityDuration.Seconds())
+	maxAge := int(a.validityDuration.Seconds())
 
 	cookie := fmt.Sprintf("%s=%s; Max-Age=%d; Secure; HttpOnly", a.cookieName, token, maxAge)
 
@@ -44,16 +44,16 @@ func (j *Authenticator) GenerateTokenCookie(id, name string) (string, error) {
 // GenerateJWT returns a new JSON Web Token for the user.
 // The token is valid for the configured validityDuration.
 // It contains provided the (user) id and name as payload.
-func (j *Authenticator) GenerateJWT(id, name string) (string, error) {
+func (a *Authenticator) GenerateJWT(id, name string) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 
 	claims["authorized"] = true
 	claims["id"] = id
 	claims["user"] = name
-	claims["exp"] = time.Now().Add(j.validityDuration).Unix()
+	claims["exp"] = time.Now().Add(a.validityDuration).Unix()
 
-	tokenString, err := token.SignedString([]byte(j.secret))
+	tokenString, err := token.SignedString([]byte(a.secret))
 	if err != nil {
 		return "", err
 	}
@@ -61,17 +61,11 @@ func (j *Authenticator) GenerateJWT(id, name string) (string, error) {
 }
 
 // Verify that the token is valid.
-func (j *Authenticator) Verify(token string) error {
-	_, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+func (a *Authenticator) Verify(token string) (*jwt.Token, error) {
+	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Could not parse token: token method is %T instead of jwt.SigningMethodHMAC", token.Method)
 		}
-		return []byte(j.secret), nil
+		return []byte(a.secret), nil
 	})
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
